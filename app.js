1a474efb891c06af95dc246a6562ba63f69a134c
Ray Britton <raybritton@gmail.com>
2019-01-19 14:49:44 +0000

    First website prototype

--- app.js
+++ app.js
@@ -3,20 +3,72 @@ require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const sprintf = require('sprintf-js').sprintf;
+const bodyParser = require('body-parser');
 const app = express();
+const util = require('util');
+var spawn = require('child-process-promise').spawn;
 
 global.SERVER_HOST = process.env.HOSTNAME || 'localhost';
 global.SERVER_PORT = process.env.PORT || '3001';
 
+const JQL_LIB = process.env.LIB_PATH || 'res/jq-0.13.2.jar'
+
 const syntaxDocPage = makeSyntaxDocPage();
+const jqlPage = fs.readFileSync('res/jql.html').toString();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/alive', (req, res) => res.sendStatus(200));
 
 app.get('/docs', (req, res) => {
 	res.send(syntaxDocPage);
 });
+
+app.get('/', (req, res) => {
+	res.send(jqlPage);
+})
+
+app.post('/', (req, res) => {
+	var input = req.body.input || "";
+	var query = req.body.query || "";
+
+	if (input == "" || query== "") {
+		res.contentType("application/json").send(JSON.stringify({error:{message: "No content"}}));
+		return;
+	}
+
+	try {
+		input = JSON.stringify(JSON.parse(input));
+	} catch (err) {
+		res.contentType("application/json").send(JSON.stringify({error:{message: "Invalid json"}}));
+		return;
+	}
+
+	spawn('java', ['-jar', JQL_LIB, '-i', input, '-q', query, '-api'], { capture: [ 'stdout' ]})
+    .then(function (result) {
+				var data = result.stdout.toString();
+				var json = {error:{extra: ""}}
+				if (data == "") {
+					json = {
+						error: {
+							message: "Unknown error"
+						}
+					}
+				} else {
+					json = JSON.parse(data);
+					json.error = json.error || {extra: ""};
+				}
+				
+				json.error.extra = (json.error.extra || "").replace(/\n/g, "<br>");
+				res.contentType("application/json").send(JSON.stringify(json));
+    })
+    .catch(function (err) {
+			console.log(err);
+			res.contentType("application/json").send(JSON.stringify({error:{message: "Unknown error"}}));
+    });
+
+})
 
 app.use(function(err, req, res, next) {
     if (res.headersSent) {
